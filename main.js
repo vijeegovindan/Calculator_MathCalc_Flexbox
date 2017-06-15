12e6acda099e750e7fa6fccace9021a95a810f99
Balaji Balakrishnan <balajibalakrishnan@balajis-air.lan>
2017-06-15 09:21:42 -0400

    resubmitted

--- main.js
+++ main.js
@@ -16,43 +16,43 @@ for (let i = 0; i < btn_List.length; i++) { //for-start
     var reg_exp_num = new RegExp('[0-9]');
     var reg_exp_op = new RegExp('[*,%,+,/,-]');
 
-
     if(reg_exp_num.test(var_btn)){ // if number, keep adding the numbers until 16
       output.innerHTML +=var_btn;
-      return false;
     }
     else if(reg_exp_op.test(var_btn)){ // if itz an operator
       output.innerHTML = (output.innerHTML === "")? output.innerHTML ="" : (output.innerHTML + var_btn);
-      return false;
     }
     else if(var_btn == "."){ // if itz a period
       output.innerHTML = (output.innerHTML === "")? "0." : (output.innerHTML + ".");
-      return false;
     }
     else if(var_btn == "C"){ // if itz cancel
       output.innerHTML = "";
-      return false;
     }
     else if(this.id == "id_sqrt"){
       output.innerHTML +=var_btn;
-      return false;
     }
     else if(var_btn == "="){ //results
-    /*  var split_num = output.innerHTML.split(/(\d)/);
-      if(split_num[0] == "&radic;")
+      if(reg_exp_op.test(output.innerHTML))
       {
-        result = Math.sqrt(split_num);
-        output.innerHTML = result;
-        return false;
-      }*/
-      result = eval(output.innerHTML);
-      if(result%1 === 0){
-        output.innerHTML = result;
+        result = eval(output.innerHTML);
+        if (result % 1 === 0){
+          output.innerHTML = result;
+        }
+        else{
+          output.innerHTML = result.toFixed(2);
+        }
       }
-      else{
-        output.innerHTML = result.toFixed(2);
-      }
+
+      var sqrt = parseInt(output.innerHTML.charCodeAt(0));
+      if(sqrt === 8730)
+      {
+        var str = output.innerHTML;
+         str1 = str.slice(1,output.innerHTML.length);
+         result = Math.sqrt(str1);
+         output.innerHTML = result.toFixed(3);
+         return false;
+       }
+       return false;
     }
-    return false;
   }); //function click-end
 } // for-end
